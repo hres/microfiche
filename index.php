4a5e9f51bed2529822311bb32065247f166edfa3
ayoung8 <ayoung8@users.noreply.github.com>
2016-06-04 19:54:59 -0400

    Update index.php

--- index.php
+++ index.php
@@ -47,7 +47,7 @@ echo " Results of '$searchelement' beginging with '$searchvalue'<hr>";
 //$queryresult = "SELECT * FROM $tablename WHERE $searchelement LIKE '$searchfor%';";
 echo $searchelement;
 echo $searchfor;
-$queryresult = "SELECT * FROM $tablename WHERE AccessNum LIKE '7537%';";
+$queryresult = "SELECT * FROM $tablename WHERE $searchelement LIKE '7537%';";
 $result = mysql_query($queryresult);
 while ($row = mysql_fetch_array($result)) {
        echo "<tr>";
